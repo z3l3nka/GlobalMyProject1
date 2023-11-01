80d351ab66fefce1c9897dbecd2cf71cfb0d9bb8
Maksim Makarenko <max.wrx@mail.ru>
2023-11-01 19:10:26 +0300

    add function MyPow()

--- funcs.h
+++ funcs.h
@@ -3,5 +3,6 @@
 double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
-double MyDiv(double a, double b)
+double MyDiv(double, double);
+int MyPow(int, int);
 #endif // __FUNCS_H__
--- programm.cpp
+++ programm.cpp
@@ -6,7 +6,8 @@ int main()
 	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
 	cout << MyMul(10, 3) << endl;
-	double MyDiv(10, 3) << endl;
+	cout << MyDiv(10, 3) << endl;
+	cout << MyPow(10, 3) << endl;
 	system("pause");
 	return 0;
 }
